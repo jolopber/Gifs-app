854afb49e79e2cf1506c2fe962beeebf59f94869
Jose Lopez <jose.lopezberro@gmail.com>
2024-12-10 10:33:57 +0100

    adiccion tarjetas en barra lateral

---
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'gifs-card-list',
+    templateUrl: 'card-list.component.html'
+})
+
+export class CardListComponent {
+}
