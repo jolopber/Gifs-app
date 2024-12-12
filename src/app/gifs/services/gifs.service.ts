d0ee9210dd73e818ed156ff529537ab71f6949bf
Jose Lopez <jose.lopezberro@gmail.com>
2024-12-12 12:24:41 +0100

    fin aplicacion gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,10 @@ export class GifsService {
     private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
 
-    constructor( private http: HttpClient ) { }
+    constructor( private http: HttpClient ) {
+        this.loadLocalStorage();
+        console.log('Gifs Service Ready')
+     }
 
 
     get tagsHistory() {
@@ -34,6 +37,15 @@ export class GifsService {
 
     private saveLocalStorage():void {
         localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+    }
+
+    private loadLocalStorage():void {
+        if ( !localStorage.getItem('history')) return;
+
+        this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+        if ( this._tagsHistory.length === 0 ) return;
+        this.searchTag( this._tagsHistory[0] );
     }
 
     searchTag( tag: string ):void {
